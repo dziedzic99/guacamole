ef11cc761f06ec219fda7e9e277282731f59cf9c
Jan Dziedzic <jandziedzic99@gmail.com>
2018-05-01 19:41:40 +0100

    OLED GUI added, altimeter is based on mock value

--- guacamole_receiver/guacamole_receiver.ino
+++ guacamole_receiver/guacamole_receiver.ino
@@ -1,6 +1,12 @@
 #include <SPI.h>
 #include <LoRa.h>
-#define INCOMING_SIZE 7
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+#include "font.h"
+#define INCOMING_SIZE 14
+
+
+SSD1306  display(0x3c, 4, 15);
+String incomingParsed [INCOMING_SIZE];
 
 void setup() {
   Serial.begin(9600);
@@ -12,6 +18,14 @@ void setup() {
 //    Serial.println("Starting LoRa failed!");
     while (1);
   }
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+  display.init();
+  display.flipScreenVertically();
+  display.clear();
+
 //  Serial.println("LoRa on!");
 //  LoRa.setSpreadingFactor(12);
 //  LoRa.setTxPower(17);
@@ -20,8 +34,43 @@ void setup() {
 //  Serial.println("LoRa config on!");
 }
 
+void dspl(){
+  display.clear();
+  display.setFont(Monospaced_plain_9); 
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(0, 0, "Num:   "+incomingParsed[0]);
+  display.drawString(0, 9, "RSSI:  "+incomingParsed[INCOMING_SIZE-1]);
+
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(128, 0, "GPS sats: "+incomingParsed[4]);
+  display.drawString(128, 9, incomingParsed[2]);
+  display.drawString(128, 18, incomingParsed[3]);
+
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  if (incomingParsed[8 ]=="0") display.drawString(0, 18, "Beep:  Off");
+  if (incomingParsed[8 ]=="1") display.drawString(0, 18, "Beep:  On");
+
+  if (incomingParsed[9 ]=="0") display.drawString(0, 27, "Light: Off");
+  if (incomingParsed[9 ]=="1") display.drawString(0, 27, "Light: On");
+
+  if (incomingParsed[10]=="0") display.drawString(0, 36, "Clamp: Off");
+  if (incomingParsed[10]=="1") display.drawString(0, 36, "Clamp: On");
+
+  display.drawString(0, 45, "Temp:  "+incomingParsed[5].substring(0,incomingParsed[5].length()-1)+"C");
+  display.drawString(0, 54, "Hum:   "+incomingParsed[6].substring(0,incomingParsed[6].length()-1)+"%");
+
+  
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(128, 36, "Altitude:");
+  display.display();
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.setFont(Monospaced_plain_18);
+  display.drawString(128, 45, "1250m");
+  display.display();
+}
+
 void send(String string) {
-  Serial.println("Sending \"" + string + "\"");
+  //Serial.println("Sending \"" + string + "\"");
   LoRa.beginPacket();
   LoRa.print(string);
   LoRa.endPacket();
@@ -39,19 +88,21 @@ void loop() {
   if (packetSize) {
     String incoming = "";
     for (int i=0; i<packetSize; i++) {
-        Serial.print(packet[i]);
+//      Serial.print(packet[i]);
         incoming += packet[i];
     }
-      Serial.print(",");
-      Serial.println(LoRa.packetRssi());
-//      Serial.println(incoming);
-      String incomingParsed [INCOMING_SIZE];
-      for (int i=0; i<INCOMING_SIZE-1; i++) {
+//    Serial.print(",");
+//    Serial.println(LoRa.packetRssi());
+    Serial.println(incoming);
+      
+      for (int i=0; i<INCOMING_SIZE-2; i++) {
         int p1 = incoming.indexOf(',');
         incomingParsed[i] = incoming.substring(0, p1);
-        incoming = incoming.substring(p1+1, incoming.length()-1);
+        incoming = incoming.substring(p1+1, incoming.length());
       }
-      incomingParsed[INCOMING_SIZE-1] = incoming;
+      Serial.println();
+      incomingParsed[INCOMING_SIZE-2] = incoming;
+      incomingParsed[INCOMING_SIZE-1] = LoRa.packetRssi();
       String outgoing = "";
       outgoing += incomingParsed[0];
       outgoing += ",";
@@ -60,7 +111,8 @@ void loop() {
       outgoing += "000";
       outgoing += ",";
       outgoing += "000";
-      //delay(10);
+      dspl();
+      delay(50);
       send(outgoing);
     }
 }
